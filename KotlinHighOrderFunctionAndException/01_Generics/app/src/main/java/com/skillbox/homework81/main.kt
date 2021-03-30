15ccd73c19538d9dc9fe4ea2bbbfd15c4f56112b
Livirinov <livirinov@gmail.com>
2021-03-30 18:25:27 +0500

    Homework 8.1 is fixed again. Types were converted

--- KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/skillbox/homework81/main.kt
+++ KotlinHighOrderFunctionAndException/01_Generics/app/src/main/java/com/skillbox/homework81/main.kt
@@ -5,17 +5,17 @@ import com.skillbox.homework81.Result.Error
 import kotlin.random.Random
 
 fun main() {
-    val listInt = listOf(1, 2, 3, 4, 5, 6)
-    val listDouble = listOf(1.0, 2.2, 3.0, 4.0, 5.0, 6.0)
+    val listLong = listOf(-0L, +0L, 1L, 2L, Long.MIN_VALUE, Long.MAX_VALUE)
+    val listDouble = listOf(-0.0, +0.0, 1.0, -2.0, 3.0, 4.0, 5.5, 6.5)
 
-    println("Whole even numbers: " + evenItems(listInt))
+    println("Whole even numbers: " + evenItems(listLong))
     println()
     println("Real even numbers: " + evenItems(listDouble))
 }
 
 fun <T: Number> evenItems(inputList: List<T>): List<T> {
     val newList = emptyList<T>().toMutableList()
-    inputList.forEach{if (it.toDouble() % 2 == 0.0) newList.add(it)}
+    inputList.forEach{if (it.toDouble() - it.toLong() == 0.0 && it.toLong() % 2 == 0L) newList.add(it)}
     return newList
 }
 
